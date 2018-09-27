4d47d7aa1cf2b685bea2ce77c896480ea2217e49
will <willyanlin@gmail.com>
2018-09-27 23:55:54 +0200

    ajout des pages addition

---
+++ menu.php
@@ -0,0 +1,18 @@
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+    <li class="nav-item">
+        <a class="nav-link" id="home-tab" href="./home.php">Home</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" id="profile-tab" href="./choix.php">Mathématiques</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" id="contact-tab" href="#contact">Français</a>
+    </li>
+</ul>
+<!--
+<div class="tab-content" id="myTabContent">
+    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">.dzedz..</div>
+    <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+</div>
+-->
