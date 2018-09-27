a8cf04705079932cae04a304d12a298ef85657be
Laurène <33252802+Laureneb27@users.noreply.github.com>
2018-09-27 16:39:40 +0200

    bdd

---
+++ ecole.sql
@@ -0,0 +1,35 @@
+
+ #Insertion final
+
+ INSERT INTO personne (Nom,Prenom,Mdp,Photo) VALUES ( 'Jean','Lario','123','chat.jpg');
+ SET @idpersonne := (SELECT LAST_INSERT_ID() ); 
+INSERT INTO eleve(Classe,IdPersonne)
+ VALUES ( 'CP',@idpersonne); 
+######
+INSERT INTO personne (Nom,Prenom,Mdp,Photo) VALUES ( 'Louis','Lasalle','123','chat.jpg');
+ SET @idpersonne := (SELECT LAST_INSERT_ID() ); 
+INSERT INTO eleve(Classe,IdPersonne)
+ VALUES ( 'CE',@idpersonne); 
+
+######
+INSERT INTO personne (Nom,Prenom,Mdp,Photo) VALUES ( 'Marc','Legre','123','chat.jpg');
+ SET @idpersonne := (SELECT LAST_INSERT_ID() ); 
+INSERT INTO eleve(Classe,TypeP,IdPersonne)
+ VALUES ( 'CP','Eleve',@idpersonne);
+ #####
+ INSERT INTO matiere(Nom) VALUES('Français');
+ INSERT INTO personne (Nom,Prenom,Mdp,Photo) VALUES ( 'Lucie','Lant','123','chat.jpg');
+ SET @idpersonne := (SELECT LAST_INSERT_ID() ); 
+INSERT INTO Professeur(IdPersonne,TypeProf,IdMatiere_Pratiquer)
+ VALUES ( @idpersonne,"Prof",'1');
+
+# Vue complete des élève
+ CREATE VIEW EleveInfo
+AS SELECT personne.IdPersonne, personne.Nom, personne.Mdp, personne.Photo, eleve.Classe
+FROM personne,eleve where personne.IdPersonne=eleve.IdPersonne;
+  
+# Vue complete des Professeur
+ CREATE VIEW ProfesseurInfo
+    AS SELECT personne.IdPersonne, personne.Nom, personne.Mdp, personne.Photo, Matiere.nom Matiere
+    FROM personne,Professeur,Matiere where personne.IdPersonne=Professeur.IdPersonne ;
+
