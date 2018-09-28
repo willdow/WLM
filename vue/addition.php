d1f4cc53a0e295672a27c75846015d0861c6bc07
will <willyanlin@gmail.com>
2018-09-28 16:51:11 +0200

    tt

--- vue/addition.php
+++ vue/addition.php
@@ -6,22 +6,31 @@
 <body>
     <?php include('../menu.php'); ?>
     <div class="container ">
-        <div class="row questionnaire welcome">
-        <form method="POST" action="./home.php">
-            <h1 class="titreQuestionnaire">Quizz 1 : Addition</h1>
-            <div class="form-group">
-                <div class="question">
-                    <p>Comment s'écrit 11 ?</p>
+    <h3 class="titreQuestionnaire">Quizz 1 : Addition</h3>
+        <div class="row questionnaire center">
+            <form method="POST" action="./home.php">
+                
+                <div class="form-group">
+                    <div class="question">
+                        <h1>Comment s'écrit 11 ?</h1>
+                    </div>
+                    <div class="reponse">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-12">
+                                    <a type="button" class="btn btnReponse">anse</a>
+                                    <a type="button" class="btn btnReponse">onze</a>
+                                </div>
+                                <div class="col-12">
+                                    <a type="button" class="btn btnReponse">onz</a>
+                                    <a type="button" class="btn btnReponse">anze</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <input class="btn btn-primary" id="valider" name="valider" type="submit" value="Je valide ma réponde !" />
                 </div>
-                <div class="reponse">
-                    <a type="button" class="btn btnReponse">anse</a>
-                    <a type="button" class="btn btnReponse">onze</a>
-                    <a type="button" class="btn btnReponse">onz</a>
-                    <a type="button" class="btn btnReponse">anze</a>
-                </div>
-                <input class="btn btn-primary" id="valider" name="valider" type="submit" value="Je valide ma réponde !" />
-            </div>
-        </form>
+            </form>
         </div>
     </div>
     <?php include('../footer.php'); ?>
